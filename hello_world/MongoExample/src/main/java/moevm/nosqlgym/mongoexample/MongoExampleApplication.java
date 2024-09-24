34bbe0e7163f2e0120d3e369b024bb09ab85e16b
Alekhd <90794297+Alikohd@users.noreply.github.com>
2024-09-24 19:55:34 +0300

    feat: add hello world demoapp for Mongo

---
+++ hello_world/MongoExample/src/main/java/moevm/nosqlgym/mongoexample/MongoExampleApplication.java
@@ -0,0 +1,13 @@
+package moevm.nosqlgym.mongoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoExampleApplication.class, args);
+    }
+
+}
